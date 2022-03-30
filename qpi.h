0dcaa600f61823113ca7fcc7dc75535ef56be1ec
Gruekan <gruekan@qubic.com>
2022-03-30 17:28:28 +0000

    Version 0.0.0
    QPI 0.0.0. Update @matthew @Alexander Chelochev @mio @Neuron99 @petiflas @Williem Da Foe @xdSphinx @spettro We are starting the lesson in ~90 minutes.Skim over the attached files before that.Try to guess what HelloWorld(Void effect) function does.

--- qpi.h
+++ qpi.h
@@ -29,6 +29,31 @@
 #define MIN_ENERGY_AMOUNT 1000000
 #define MAX_ENERGY_AMOUNT 9223372036854775807
 
+long long add(long long a, long long b) {
+
+	return a + b;
+}
+
+long long sub(long long a, long long b) {
+
+	return a + b;
+}
+
+long long mul(long long a, long long b) {
+
+	return a * b;
+}
+
+long long div(long long a, long long b) {
+
+	return b == 0 ? 0 : a / b;
+}
+
+long long mod(long long a, long long b) {
+
+	return b == 0 ? 0 : a % b;
+}
+
 struct System {
 
 	unsigned long long	timestamp;		// [0..9223372036854775807]
@@ -48,53 +73,56 @@ struct System {
 	unsigned int		nanosecond;		// [0..999999999]
 
 	char*				self;			// Non-zero value
-};
+} system;
 
 char* computorId(
-	unsigned short computorIndex,
-	unsigned short epoch
-) {
+	unsigned short computorIndex, // Returns NULL_ID for "676", use "adminId()" to get the identity of the admin
+	unsigned short epoch // Must be "system.epoch" now
+) { // Returns the identity of the computor or NULL_ID
 
 	return nullptr;
 }
 
 char* adminId(
-) {
+) { // Returns the identity of the admin
 
 	return nullptr;
 }
 
 unsigned long long random(
-) { // Returns a deterministic pseudo-random number covering the whole unsigned 64-bit range
+) { // Returns a deterministic pseudo-random number in [0..18446744073709551615] range
 
 	return 0;
 }
 
-long long send(
-	char* destination, // The destination to send to, use NULL_ID to broadcast
-	unsigned long long timestamp, // In the most cases "State.timestamp" suits well enough
-	unsigned long long messageSize,
-	char* message
+long long send( // Attempts to send a message
+	void* destination, // The destination to send to, use NULL_ID to broadcast
+	unsigned long long timestamp, // In the most cases "system.timestamp" suits well enough
+	unsigned long long messageSize, // Message size in bytes
+	void* message // Message "struct"
 ) {
 
 	return 0;
 }
 
 long long transfer( // Attempts to transfer energy from this qubic
-	char* destination, // The destination to transfer to, use NULL_ID to destroy the transferred energy
-	unsigned long long timestamp, // In the most cases "State.timestamp" suits well enough
+	void* destination, // The destination to transfer to, use NULL_ID to destroy the transferred energy
+	unsigned long long timestamp, // In the most cases "system.timestamp" suits well enough
 	long long amount // The amount of energy to transfer, must be in [MIN_ENERGY_AMOUNT..MAX_ENERGY_AMOUNT] range despite of being a signed type
 ) { // Returns the remaining amount of energy; if the value is less than 0 then the transfer has failed, in this case the absolute value equals to the insufficient amount
 
 	return 0;
 }
 
-long long affect(
-	char* destination,
-	unsigned long long timestamp, // In the most cases "State.timestamp" suits well enough
-	unsigned long long effectSize,
-	char* effect
+long long affect( // Attempts to affect all entities in an environment
+	void* destination, // The destination environment to affect, NULL_ID is reserved
+	unsigned long long timestamp, // In the most cases "system.timestamp" suits well enough
+	unsigned long long effectSize, // Effect size in bytes
+	void* effect // Effect "struct"
 ) {
 
 	return 0;
 }
+
+struct Void {
+};
